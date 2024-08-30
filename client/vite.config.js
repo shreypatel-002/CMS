98c0c279d321b72145ee64c92290c944a66d049c
shrey patel <shreypatel295@gmail.com>
2024-08-30 17:08:35 +0530

    file structure change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +16,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [react(),vercel()],
+  plugins: [react()],
 });
